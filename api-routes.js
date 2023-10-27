bff5b3aea3b827aa59a297b77904e3dadc61ce22
schorij23 <schorij23@gmail.com>
2023-10-26 22:26:56 -0400

    after office hours and tutor

---
+++ api-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const dbJSON = require('./db.json')
+
+router.get('/api/notes', (req, res) => res.json());
+
+
+
+
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const html_routes = require('./html-routes')
 const api_routes = require('./api-routes')
 
-const app = express();
 
+const app = express();
+const PORT = 3001;
+
+app.use(html_routes);
+app.use(api_routes);
 
 
 app.listen(PORT, () =>
