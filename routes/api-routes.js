39401036de16563490043cd83948f6ad61b1c657
schorij23 <schorij23@gmail.com>
2023-10-30 11:43:22 -0400

    after tutor < error?

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,34 +1,28 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 //Universally Unique Identifier generates unique identifiers
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs'); 
 
 //GET /api/notes should read the db.json file and return all saved notes as JSON
 router.get('/api/notes', (req, res) => {
-    fs.readFile('./db.json', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.json('Note not found');
-        }
-        const notes = JSON.parse(data);
-        res.json(notes);
-    });
-});
+    fs.readFile()
+})
+    
 
 //POST /api/notes should receive a new note to save on the request body,
 //add it to the db.json file, and then return the new note to the client.
 
 router.post('/api/notes', (req, res) => {
     // Read db.json existing notes, handle errors
-    fs.readFile('./db.json', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
-            return res.json('Note not found');
+            return res.status(500).json('Note not found');
         }
-        // Parse the JSON data into a JavaScript object that represents a note
-        const notes = JSON.parse(data);
-        // Unique id for the note
+
+            // Parse the JSON data into a JavaScript object that represents a note
+            const notes = JSON.parse(data);
+        // const newNoteId = uuidv4();
         const newNoteId = uuidv4();
         // New note with request body an ID
         const newNote = {
@@ -36,14 +30,15 @@ router.post('/api/notes', (req, res) => {
             text: req.body.text,
             id: newNoteId
         };
+        
         // Adds a new note to the note array
         notes.push(newNote);
 
         // Write the new note to the database
-        fs.writeFile('./db.json', JSON.stringify(notes), err => {
+        fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
             if (err) {
                 console.error(err);
-                return res.json('Note not saved');
+                return res.status(500).json('Note not saved');
             }
             // Returns the new note
             res.json(newNote);
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const html_routes = require('./html-routes')
-const api_routes = require('./api-routes')
+const html_routes = require('./routes/html-routes')
+const api_routes = require('./routes/api-routes')
 
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use(html_routes);
 app.use(api_routes);
 
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
