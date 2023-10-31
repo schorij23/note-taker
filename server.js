643190c6ecd90927c11237975b4a279feea30d66
schorij23 <schorij23@gmail.com>
2023-10-31 08:53:26 -0400

    after office hours

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/api', api_routes);
 app.use('/', html_routes);
